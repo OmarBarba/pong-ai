beb1cfb5b00c9623ccd21f0cf30502f7fb81935a
Omar Alejandro Barba Limón <84804799+OmarBarba@users.noreply.github.com>
2023-12-01 00:50:29 -0600

    update

--- pongsolo.py
+++ pongsolo.py
@@ -32,8 +32,39 @@ class PongGame:
 
 
 
-width, heigth = 700, 500
-window = pygame.display.set_mode((width,heigth))
+def eval_genomes(genomes, config):
+    width, height = 700, 500
+    window = pygame.display.set_mode((width, height))
 
-game = Game(window, width, heigth)
+    for i, (genome_id1, genome1) in enumerate(genomes):
+        if i == len(genomes) - 1:
+            break
+        genome1.fitness = 0
+        for genome_id2, genome2 in genomes[i+1:]:
+            genome2.fitness = 0 if genome2.fitness == None else genome2.fitness
+            game = PongGame(window, width, height)
+            game.train_ai(genome1, genome2, config)
+
+def run_neat(config):
+    p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-7')
+    #p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(1))
+
+    winner = p.run(eval_genomes, 1)
+    with open("best.pickle", "wb") as f:
+        pickle.dump(winner, f)
+        
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+    # run_neat(config)
+    test_ai(config)
 
